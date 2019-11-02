defd14354edca5388bf087c430f4555ededfa05e
Parker Thompson <park.thompson@gmail.com>
2019-11-02 16:17:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function takeANumber(katzDeliLine, name) {
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length > 0) {
     return `Currently serving ${katzDeliLine[0]}.`
-    katzDeliLine[0].delete;
+    katzDeliLine.shift;
   } else {
       return 'There is nobody waiting to be served!'
   }
