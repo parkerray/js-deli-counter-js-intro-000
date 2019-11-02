57721ce153924dac3ae21dceef95cfaee999f8ff
Parker Thompson <park.thompson@gmail.com>
2019-11-02 16:05:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var katzDeliLine = [];
 let lineCount = katzDeliLine.length;
 function takeANumber(lineCount, name) {
-  katzDeliLine.push(name);
+  katzDeliLine[lineCount] = name;
   return `Welcome, ${name}. You are number ${lineCount+1} in line.`
 }
