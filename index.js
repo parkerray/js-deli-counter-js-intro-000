987b4ae32ec48e1ab00ec33e4fb18c0ae5fa7be4
Parker Thompson <park.thompson@gmail.com>
2019-11-02 16:09:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var katzDeliLine = [];
+
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   let lineCount = katzDeliLine.length;
   return `Welcome, ${name}. You are number ${lineCount} in line.`
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    return katzDeliLine[0]
+  } else {
+      return 'There is nobody waiting to be served!'
+  }
 }
